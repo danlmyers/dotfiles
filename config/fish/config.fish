bb9b570b6694b8ab818b3435baf5fafea0d9caf0
Dan Myers <dan@danlmyers.com>
2020-04-08 20:08:38 -0700

    modified fish paths to be more like original bashrc

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,2 +1,13 @@
 set fish_function_path $fish_function_path "/usr/lib/python3.8/site-packages/powerline/bindings/fish"
+eval (python -m virtualfish)
 powerline-setup
+
+if test -d ~/.local/bin
+    set PATH ~/.local/bin $PATH
+end
+if test -d ~/.bin
+    set PATH ~/.bin $PATH
+end
+if test -d ~/.pbin
+    set PATH ~/.pbin $PATH
+end
